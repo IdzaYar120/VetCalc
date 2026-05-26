c89786b21b5843006d0980d456a1cd22525fbfbe
Yevhen Kozak <yevhen.kozak6518@gmail.com>
2026-05-26 15:51:51 +0300

    fix(pwa): resolve favicon.ico 404 error with RedirectView and explicit head link tag

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from calculator import views
 
 app_name = 'calculator'
@@ -8,4 +9,5 @@ urlpatterns = [
     path('manifest.json', views.pwa_manifest, name='pwa_manifest'),
     path('service-worker.js', views.pwa_service_worker, name='pwa_service_worker'),
     path('vet_logo.svg', views.pwa_logo, name='pwa_logo'),
+    path('favicon.ico', RedirectView.as_view(url='/vet_logo.svg', permanent=True)),
 ]
