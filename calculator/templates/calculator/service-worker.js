6eacbdb9def28e606b3ab1edcc9e14e1584e183d
Yevhen Kozak <yevhen.kozak6518@gmail.com>
2026-05-26 15:55:01 +0300

    perf(pwa): localize Lucide Icons library to achieve 100% offline independence

--- calculator/templates/calculator/service-worker.js
+++ calculator/templates/calculator/service-worker.js
@@ -4,7 +4,7 @@ const ASSETS_TO_CACHE = [
     '/manifest.json',
     '/vet_logo.svg',
     'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&family=Roboto+Mono:wght@500;600;700&display=swap',
-    'https://unpkg.com/lucide@latest'
+    '/static/calculator/js/lib/lucide.min.js'
 ];
 
 // Install Service Worker and cache essential shell resources
